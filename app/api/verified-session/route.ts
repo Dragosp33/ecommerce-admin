db14b956df2d924475c38de6a7c8d5448b0841a2
Dragosp33 <dragos.polifronie@s.unibuc.ro>
2024-08-10 12:09:45 +0300

    verified-session api endpoint

---
+++ app/api/verified-session/route.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(request: NextRequest) {
+  // Create a response with a redirect
+  console.log('GET VERIFIED-SESSION::');
+  const response = NextResponse.redirect('/dashboard');
+
+  // Extract cookies from the request if needed
+  const cookies = request.cookies.getAll();
+  console.log('Cookies received:', cookies);
+
+  cookies.forEach((cookie) => {
+    response.cookies.set(cookie.name, cookie.value, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      path: '/',
+      //sameSite: 'none', // Allows the cookie to be sent cross-origin
+      //maxAge: 60 * 60 * 24 * 7, // 1 week
+    });
+  });
+
+  return response;
+}
